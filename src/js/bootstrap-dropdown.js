859addb8f18f9d89462eb80c372aa36a627783dd
Sardar <104762151+Sardar55-66@users.noreply.github.com>
2024-05-30 17:24:15 +0300

    fix

--- src/js/bootstrap-dropdown.js
+++ src/js/bootstrap-dropdown.js
@@ -1,7 +1,23 @@
 $('.dropdown-toggle').dropdown()
 
-const container = document.querySelector('.selected-dropdown-item')
+const container = document.querySelector('.selected-dropdown-item');
+const wallet = document.querySelector('.wallet-address');
+const paypalInputField = document.querySelector('.second-input-paypal')
+const nonePaypalInputField = document.querySelector('.second-input')
 
 $('.dropdown-menu').on('click', function (e) {
-    container.textContent = e.target.textContent
+
+  if (e.target.textContent === 'Pay pal') {
+    wallet.style.display = 'none';
+    paypalInputField.classList.remove('paypal-inactive')
+    nonePaypalInputField.classList.add('paypal-inactive')
+  } 
+  
+  if (e.target.textContent !== 'Pay pal') {
+    wallet.style.display = 'block';
+    paypalInputField.classList.add('paypal-inactive')
+    nonePaypalInputField.classList.remove('paypal-inactive')
+  }
+
+    container.textContent = e.target.textContent;
   })
