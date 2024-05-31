2190a7f479f5553f9645cb89e62fa6c00d43cdb8
Sardar <104762151+Sardar55-66@users.noreply.github.com>
2024-05-31 15:55:12 +0300

    last

--- src/js/bootstrap-dropdown.js
+++ src/js/bootstrap-dropdown.js
@@ -4,6 +4,7 @@ const container = document.querySelector('.selected-dropdown-item');
 const wallet = document.querySelector('.wallet-address');
 const paypalInputField = document.querySelector('.second-input-paypal')
 const nonePaypalInputField = document.querySelector('.second-input')
+const form = document.querySelector('.deposit-payment')
 
 $('.dropdown-menu').on('click', function (e) {
 
@@ -11,12 +12,15 @@ $('.dropdown-menu').on('click', function (e) {
     wallet.style.display = 'none';
     paypalInputField.classList.remove('paypal-inactive')
     nonePaypalInputField.classList.add('paypal-inactive')
+    form.style.height = '75%'
   } 
   
-  if (e.target.textContent !== 'Pay pal') {
+  // if (e.target.textContent !== 'Pay pal') 
+  else { 
     wallet.style.display = 'block';
     paypalInputField.classList.add('paypal-inactive')
     nonePaypalInputField.classList.remove('paypal-inactive')
+    form.style.height = '140px'
   }
 
     container.textContent = e.target.textContent;
